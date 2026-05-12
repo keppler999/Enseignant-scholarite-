a6122d3e8c86e488605296934c501c2adaa72348
Spirale Agence <erikaekepler@gmail.com>
2026-05-12 23:56:55 +0100

    Delete js/config.js

--- js/config.js
+++
@@ -1,5 +0,0 @@
-// Configuration Supabase
-const SB_URL = "https://icmliupsyomblifvkezh.supabase.co";
-const SB_KEY = "sb_publishable_kLQJPXQRqLKbpx1KP0be7Q_zHaCzO9c";
-
-const supabase = supabase.createClient(SB_URL, SB_KEY);
