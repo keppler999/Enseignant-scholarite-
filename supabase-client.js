9fa170c51e7f90326b2052e2d4ce5fccea8d75a6
Spirale Agence <erikaekepler@gmail.com>
2026-05-12 22:58:52 +0100

    Delete supabase-client.js

--- supabase-client.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://cpkmsuokwcymgwzqqrxz.supabase.co';
-const supabaseKey = 'sb_publishable_5Oia6PSB5xK9GwxotVd_MA_Q7lDLA03';
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
